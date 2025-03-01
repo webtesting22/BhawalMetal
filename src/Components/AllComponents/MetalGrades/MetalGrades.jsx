766e35ff64ac330689e78903af672d5e331ac7e2
webtesting22 <148150265+webtesting22@users.noreply.github.com>
2025-03-01 11:47:10 +0530

    Updated Images

--- src/Components/AllComponents/MetalGrades/MetalGrades.jsx
+++ src/Components/AllComponents/MetalGrades/MetalGrades.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import "./MetalGrades.css"
+import { Row, Col,Image } from "antd";
 const MetalGrades = () => {
     return (
         <>
@@ -20,8 +21,58 @@ const MetalGrades = () => {
                 </div>
             </div>
             <section>
+                <div id="MetalgradesContainer">
+                    <div>
+                        <p>At Bhawal Metal Industries, We have kept our niche strictly to Stainless Steel and with time we are parallelly Integrating our Products in the Stainless Steel materials as per the market needs and our clients requirements, Adding more Grades in our basket. For Now the Company's dealing in Materials could be Divided in Three Segments -  </p>
+                    </div>
+                    <div>
+                        <div className="containerGap">
+                            <h2 className="BigHeading textCenter">Austenitic Steel </h2>
+                        </div>
+                        <div>
+                            <Row>
 
-            </section>
+                                <Col lg={12}>
+                                    <div className="AboutUsContentContainer">
+                                        <div>
+                                            <p>Austenitic steels are non-magnetic stainless steels that contain a high level of chromium & nickel with a low level of carbon content. Austenitic steels are the most widely used grade of stainless steel; the most common composition is 18% Cr and 8% Ni i.e. 18/8-steels.</p>
+                                            <br />
+                                            <p>Austenitic steels are generally classified into two groups –the Chromium-Nickel AISI 300 series type and Chromium-Nickel- Manganese 200 series type. Steel with better resistance to corrosion is created by adding 2-3% molybdenum, which is often called “acid-proof steel”. Grade 304 is the most common grade of this type, which typically contains 18 percent chromium and 8 percent nickel.</p>
+                                            <br />
+                                            <p>Bhawal Metal Industries works with all materials in this grade – Bright Round Bars, PSQ Bars, Flat Bars, HR Bars & RCS, Cold Drawn Round/Flat/Square/Hexagon Bars, Forged Components and Precision Component</p>
+
+                                        </div>
+                                    </div>
+                                </Col>
+                                <Col lg={12}>
+                                    <div>
+                                        <div className="AboutUsImageContainer">
+                                            <img src="/Images/RoundsBarImage.png" alt="" loading="lazy" />
+                                        </div>
+                                    </div>
+                                </Col>
+                            </Row>
+                        </div>
+                    </div>
+                    <div>
+                        <div className="containerGap">
+                            <h2 className="BigHeading textCenter">Chemical Properties of Austenitic Steel</h2>
+                        </div>
+                        <div className="GraphImage">
+                            <Image src="/Images/ChemicalPropertiesofAusteniticSteel.png" alt="" />
+                        </div>
+                    </div>
+                    <div>
+                        <div className="containerGap">
+                            <h2 className="BigHeading textCenter">Mechanical Properties of Austenitic Steel</h2>
+                        </div>
+                        <div className="GraphImage">
+                            <Image src="/Images/MechanicalPropertiesofAusteniticSteel.png" alt="" />
+                        </div>
+                    </div>
+
+                </div>
+            </section >
         </>
     )
 }
