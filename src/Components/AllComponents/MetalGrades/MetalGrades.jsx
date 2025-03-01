f44578fc5eea305770e3c315143caa0fbe21e601
webtesting22 <148150265+webtesting22@users.noreply.github.com>
2025-03-01 11:48:12 +0530

    Update MetalGrades.jsx

--- src/Components/AllComponents/MetalGrades/MetalGrades.jsx
+++ src/Components/AllComponents/MetalGrades/MetalGrades.jsx
@@ -35,7 +35,7 @@ const MetalGrades = () => {
                                 <Col lg={12}>
                                     <div className="AboutUsContentContainer">
                                         <div>
-                                            <p>Austenitic steels are non-magnetic stainless steels that contain a high level of chromium & nickel with a low level of carbon content. Austenitic steels are the most widely used grade of stainless steel; the most common composition is 18% Cr and 8% Ni i.e. 18/8-steels.</p>
+                                            <p><b>Austenitic steels</b> are non-magnetic stainless steels that contain a high level of chromium & nickel with a low level of carbon content. Austenitic steels are the most widely used grade of stainless steel; the most common composition is 18% Cr and 8% Ni i.e. 18/8-steels.</p>
                                             <br />
                                             <p>Austenitic steels are generally classified into two groups –the Chromium-Nickel AISI 300 series type and Chromium-Nickel- Manganese 200 series type. Steel with better resistance to corrosion is created by adding 2-3% molybdenum, which is often called “acid-proof steel”. Grade 304 is the most common grade of this type, which typically contains 18 percent chromium and 8 percent nickel.</p>
                                             <br />
